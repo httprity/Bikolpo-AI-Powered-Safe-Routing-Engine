8354b63b9591888b664c3550ffe6853a8b64de02
httprity <sampritychess@gmail.com>
2025-11-26 22:34:43 +0600

    Initial commit of Bikolpo (frontend, backend, ML pipeline)

--- config.py
+++ config.py
@@ -2,16 +2,20 @@
 Configuration file for FloodSafe ML Pipeline
 Store your API credentials here (add to .gitignore!)
 """
+import os
 
 # NASA Earthdata credentials
-NASA_USERNAME = "samprity"  # Replace with your NASA Earthdata username
-NASA_PASSWORD = "Shampoo2015!"  # Replace with your NASA Earthdata password
+NASA_USERNAME = os.getenv("NASA_USERNAME")
+NASA_PASSWORD = os.getenv("NASA_PASSWORD")
+
 
 # Google Earth Engine
-GEE_SERVICE_ACCOUNT_JSON = r"C:\Users\utech\Downloads\testing-477914-273b428b8bc2.json"  # Path to your GEE JSON key file
+GEE_SERVICE_ACCOUNT_JSON = os.getenv("GEE_SERVICE_ACCOUNT_JSON")
+
 
 # OpenRouteService (already have)
-ORS_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijg2MmVmMzA4NmVhNDQ3MThiNmU0NGQwODQ1MjU5NzNiIiwiaCI6Im11cm11cjY0In0="
+ORS_API_KEY =  os.getenv("ORS_API_KEY")
+
 
 # Data storage paths
 DATA_DIR = "data"
@@ -19,11 +23,14 @@ RAW_DATA_DIR = f"{DATA_DIR}/raw"
 PROCESSED_DATA_DIR = f"{DATA_DIR}/processed"
 MODEL_DIR = "models"
 
+
 # Model parameters
 RANDOM_STATE = 42
 TEST_SIZE = 0.2
+
 
 # Routing parameters
 LAMBDA_SAFEST = 10.0  # High weight on flood risk
 LAMBDA_BALANCED = 3.0  # Medium weight
 LAMBDA_FASTEST = 0.5  # Low weight (prioritize speed)
+
